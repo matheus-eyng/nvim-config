bc492272a7df7b9305a1e0108ef42d0f5660e7e7
Matheus Eyng de França <matheuseyngsf@gmail.com>
2022-05-18 22:27:56 -0300

    Add more stuff, completion

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require "matheus.options" -- `lua.` is implied
 require "matheus.keymaps"
 require "matheus.plugins"
+require "matheus.cmp"
 require "matheus.colorscheme"
--- lua/matheus/plugins.lua
+++ lua/matheus/plugins.lua
@@ -46,6 +46,18 @@ return packer.startup(function(use)
     use "nvim-lua/plenary.nvim" -- useful lua functions used by lots of plugins
     use "lunarvim/colorschemes"
 
+    -- cmp plugins
+    use "hrsh7th/nvim-cmp" -- The completion plugin
+    use "hrsh7th/cmp-buffer" -- buffer completions
+    use "hrsh7th/cmp-path" -- path completions
+    use "hrsh7th/cmp-cmdline" -- cmdline completions
+    use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+    -- snippets
+    use "L3MON4D3/LuaSnip" --snippet engine
+    use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+
     if PACKER_BOOSTRAP then
         require("packer").sync()
     end
