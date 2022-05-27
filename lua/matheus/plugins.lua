ea4ff98d14f4e0bb34d461a6b638a7f226d99a6a
Matheus Eyng de França <matheuseyngsf@gmail.com>
2022-05-26 21:30:15 -0300

    Add treesitter

--- lua/matheus/plugins.lua
+++ lua/matheus/plugins.lua
@@ -67,6 +67,14 @@ return packer.startup(function(use)
     use "nvim-telescope/telescope.nvim"
     use 'nvim-telescope/telescope-media-files.nvim'
 
+    -- Treesitter
+    use {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+    }
+    use "p00f/nvim-ts-rainbow"
+    use "nvim-treesitter/playground"
+
     if PACKER_BOOSTRAP then
         require("packer").sync()
     end
